b99790e517bd0234644bba60e8ad80a4904818de
NirmalHettiarachchi <nirmalhettiarachchi5@gmail.com>
2023-11-10 09:47:27 +0530

    Changes to the customer dashboard

--- app/src/main/java/eu/tutorials/roadrescuecustomer/Dashboard.kt
+++ app/src/main/java/eu/tutorials/roadrescuecustomer/Dashboard.kt
@@ -4,6 +4,7 @@ import android.graphics.drawable.shapes.Shape
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -13,15 +14,21 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowForward
 import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.icons.filled.PlayArrow
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -36,8 +43,36 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
+val textStyle1 = TextStyle(
+    fontWeight = FontWeight.ExtraBold,
+    fontSize = 20.sp,
+    letterSpacing = 0.15.sp,
+    color = Color(0xFF253555)
+)
+
+val textStyle2 = TextStyle(
+    fontWeight = FontWeight.ExtraBold,
+    fontSize = 16.sp,
+    letterSpacing = 0.15.sp,
+    color = Color(0xFF253555),
+)
+
+val textStyle3 = TextStyle(
+    fontWeight = FontWeight.ExtraBold,
+    fontSize = 16.sp,
+    letterSpacing = 0.15.sp,
+    color = Color.White,
+)
+
+val cardModifier = Modifier
+    .fillMaxWidth()
+    .padding(16.dp)
+    .background(color = Color(0xFFB6C7E3), shape = RoundedCornerShape(percent = 10))
+    .border(width = 2.dp, color = Color.White, shape = RoundedCornerShape(percent = 10))
+
 @Composable
 fun Dashboard() {
+
     Column (
         modifier = Modifier
             .fillMaxSize()
@@ -54,112 +89,138 @@ fun Dashboard() {
         Text(
             text = "Welcome Nirmal Hettiarachchi",
             modifier = Modifier.align(Alignment.CenterHorizontally),
-            style = TextStyle(
-                fontWeight = FontWeight.ExtraBold,
-                fontSize = 20.sp,
-                letterSpacing = 0.15.sp,
-                color = Color(0xFF253555)
-            )
+            style = textStyle1
         )
 
-        //Request Service Box
-        Card(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(16.dp)
-                .background(color = Color(0xFFB6C7E3), shape = RoundedCornerShape(percent = 10))
-                .border(width = 2.dp, color = Color.White, shape = RoundedCornerShape(percent = 10)),
-            elevation = CardDefaults.cardElevation(defaultElevation = 4.dp),
-            colors = CardDefaults.cardColors(containerColor = Color(0xFFB6C7E3))// Apply shadow to the outer Box
-        ) {
-            Column(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(4.dp)
-            ) {
-                Spacer(modifier = Modifier.height(16.dp))
-                Text(
-                    text = "Have you faced a vehicle breakdown? Just click on the button below...",
-                    style = TextStyle(
-                        fontWeight = FontWeight.ExtraBold,
-                        fontSize = 16.sp,
-                        letterSpacing = 0.15.sp,
-                        color = Color(0xFF253555),
-                        textAlign = TextAlign.Center
-                    )
-                )
-                Spacer(modifier = Modifier.height(16.dp))
-                Button(
-                    modifier = Modifier.align(Alignment.CenterHorizontally),
-                    onClick = {},
-                    border = BorderStroke(width = 2.dp, color = Color.White),
-                    elevation = ButtonDefaults.buttonElevation(defaultElevation = 4.dp),
-                    colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF253555))
-                ) {
-                    Row(
-                        verticalAlignment = Alignment.CenterVertically,
-                        horizontalArrangement = Arrangement.spacedBy(4.dp) // Adjust spacing between icon and text as needed
-                    ) {
-                        Icon(
-                            imageVector = Icons.Default.ArrowForward, // Replace 'YourIcon' with the desired icon from the Icons.Default class
-                            contentDescription = null, // Provide a content description for accessibility (can be null if not needed)
-                            tint = Color.White // Tint color of the icon
-                        )
-                        Text(
-                            text = "Request Service",
-                            style = TextStyle(
-                                fontWeight = FontWeight.ExtraBold,
-                                fontSize = 16.sp,
-                                letterSpacing = 0.15.sp,
-                                color = Color.White,
-                            )
-                        )
-                    }
-                }
-                Spacer(modifier = Modifier.height(16.dp))
-            }
-        }
-
-        Box(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(16.dp)
-                .shadow(4.dp) // Apply shadow to the outer Box
-        ) {
-            Box(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .border(
-                        border = BorderStroke(2.dp, Color.White),
-                        shape = RoundedCornerShape(percent = 10),
-                    )
-                    .background(color = Color(0xFFB6C7E3))
-                    .padding(4.dp)
-            ) {
-                //common vehicle breakdown box
-                Text(text = "/////Box for the common vehicle breakdowns")
-            }
-        }
-
-        Box(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(16.dp)
-                .shadow(4.dp) // Apply shadow to the outer Box
-        ) {
-            Box(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .border(
-                        border = BorderStroke(2.dp, Color.White),
-                        shape = RoundedCornerShape(percent = 10),
-                    )
-                    .background(color = Color(0xFFB6C7E3))
-                    .padding(4.dp)
-            ) {
-                //hrlp box
-                Text(text = "/////This is the hep box...")
-            }}
+        RequestServiceBox()
+        CommonIssuesBox()
+        HelpBox()
     }
 }
 
+@Composable
+fun RequestServiceBox() {
+    Card(
+        modifier = cardModifier,
+        elevation = CardDefaults.cardElevation(defaultElevation = 4.dp),
+        colors = CardDefaults.cardColors(containerColor = Color(0xFFB6C7E3))// Apply shadow to the outer Box
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(4.dp)
+        ) {
+            Spacer(modifier = Modifier.height(16.dp))
+            Text(
+                text = "Have you faced a vehicle breakdown?",
+                modifier = Modifier.align(Alignment.CenterHorizontally),
+                style = textStyle2
+            )
+            Text(
+                text = "Just click on the button below...",
+                modifier = Modifier.align(Alignment.CenterHorizontally),
+                style = textStyle2
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+            Button(
+                modifier = Modifier.align(Alignment.CenterHorizontally),
+                onClick = {},
+                border = BorderStroke(width = 2.dp, color = Color.White),
+                elevation = ButtonDefaults.buttonElevation(defaultElevation = 4.dp),
+                colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF253555))
+            ) {
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.spacedBy(4.dp) // Adjust spacing between icon and text
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.ArrowForward,
+                        contentDescription = null,
+                        tint = Color.White // Tint color of the icon
+                    )
+                    Text(
+                        text = "Request Service",
+                        style = textStyle3
+                    )
+                }
+            }
+            Spacer(modifier = Modifier.height(16.dp))
+        }
+    }
+}
+
+@Composable
+fun CommonIssuesBox() {
+    Card(
+        modifier = cardModifier,
+        elevation = CardDefaults.cardElevation(defaultElevation = 4.dp),
+        colors = CardDefaults.cardColors(containerColor = Color(0xFFB6C7E3))// Apply shadow to the outer Box
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(4.dp)
+        ) {
+            Spacer(modifier = Modifier.height(16.dp))
+            Text(
+                text = "Common vehicle breakdown types",
+                modifier = Modifier.align(Alignment.CenterHorizontally),
+                style = textStyle2
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+            Button(
+                modifier = Modifier.align(Alignment.CenterHorizontally),
+                onClick = {},
+                border = BorderStroke(width = 2.dp, color = Color.White),
+                elevation = ButtonDefaults.buttonElevation(defaultElevation = 4.dp),
+                colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF253555))
+            ) {
+                Text(
+                    text = "Request Service",
+                    style = textStyle3
+                )
+            }
+            Spacer(modifier = Modifier.height(16.dp))
+        }
+    }
+}
+
+@Composable
+fun HelpBox() {
+    Card(
+        modifier = cardModifier,
+        elevation = CardDefaults.cardElevation(defaultElevation = 4.dp),
+        colors = CardDefaults.cardColors(containerColor = Color(0xFFB6C7E3))// Apply shadow to the outer Box
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(4.dp)
+        ) {
+            Spacer(modifier = Modifier.height(6.dp))
+            Row(
+                modifier = Modifier
+                    .padding(8.dp)
+                    .fillMaxWidth()
+                    .align(Alignment.CenterHorizontally),
+                horizontalArrangement = Arrangement.Center
+            ) {
+                Text(
+                    text = "How to use the system?",
+                    style = textStyle2,
+                    modifier = Modifier.align(Alignment.CenterVertically)
+                )
+                Spacer(modifier = Modifier.width(16.dp))
+                IconButton(onClick = {}) {
+                    Icon(
+                        imageVector = Icons.Default.PlayArrow,
+                        contentDescription = null,
+                        tint = Color(0xFF253555),
+                        modifier = Modifier.size(24.dp)
+                    )
+                }
+            }
+            Spacer(modifier = Modifier.height(6.dp))
+        }
+    }
+}
