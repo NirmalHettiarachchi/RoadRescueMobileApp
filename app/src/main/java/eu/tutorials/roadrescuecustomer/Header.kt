002a9c045439aa07069f3bc9ddd0b53176f1c03d
NirmalHettiarachchi <nirmalhettiarachchi5@gmail.com>
2023-11-11 08:06:12 +0530

    Separated the header, footer and help box

---
+++ app/src/main/java/eu/tutorials/roadrescuecustomer/Header.kt
@@ -0,0 +1,45 @@
+package eu.tutorials.roadrescuecustomer
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun Header() {
+    CenterAlignedTopAppBar(
+        title = {
+            Icon(
+                painter = painterResource(id = R.drawable.logo),
+                modifier = Modifier.size(100.dp),
+                tint = Color.Unspecified,
+                contentDescription = "Toolbar icon"
+            )
+        },
+        navigationIcon = {
+            IconButton(onClick = { }) {
+                Icon(
+                    imageVector = Icons.Filled.Menu,
+                    tint = Color.White,
+                    modifier = Modifier.size(30.dp),
+                    contentDescription = "Localized description"
+                )
+            }
+        },
+        modifier = Modifier.fillMaxWidth(),
+        colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+            containerColor = Color(0xFF253555)
+        ),
+    )
+}
