2c74f3ab3ef14329022eb414f4258c1b0806ee9f
NirmalHettiarachchi <nirmalhettiarachchi5@gmail.com>
2023-11-11 09:34:22 +0530

    Created a separate file for text styles and modifiers

--- app/src/main/java/eu/tutorials/roadrescuecustomer/Dashboard.kt
+++ app/src/main/java/eu/tutorials/roadrescuecustomer/Dashboard.kt
@@ -1,7 +1,6 @@
 package eu.tutorials.roadrescuecustomer
 
 import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.IntrinsicSize
@@ -17,60 +16,20 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 
-val textStyle1 = TextStyle(
-    fontWeight = FontWeight.ExtraBold,
-    fontSize = 20.sp,
-    letterSpacing = 0.15.sp,
-    color = Color(0xFF253555)
-)
-
-val textStyle2 = TextStyle(
-    fontWeight = FontWeight.ExtraBold,
-    fontSize = 16.sp,
-    letterSpacing = 0.15.sp,
-    color = Color(0xFF253555),
-)
-
-val textStyle3 = TextStyle(
-    fontWeight = FontWeight.ExtraBold,
-    fontSize = 16.sp,
-    letterSpacing = 0.15.sp,
-    color = Color.White,
-)
-
-val cardModifier = Modifier
-    .fillMaxWidth()
-    .padding(horizontal = 16.dp, vertical = 8.dp)
-
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Dashboard() {
     Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(
-                brush = Brush.linearGradient(
-                    colors = listOf(Color(0xFFD3EFFF), Color.White),
-                    start = Offset(0f, 0f),
-                    end = Offset(0f, Float.POSITIVE_INFINITY)
-                )
-            ),
+        backgroundModifier,
         verticalArrangement = Arrangement.SpaceBetween,
     ) {
         Column {
@@ -82,7 +41,6 @@ fun Dashboard() {
                 modifier = Modifier.align(Alignment.CenterHorizontally),
                 style = textStyle1
             )
-
             RequestServiceBox()
             CommonIssuesBox()
             HelpBox()
