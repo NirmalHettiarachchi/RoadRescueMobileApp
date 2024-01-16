68659a091c4a648ec61b62ea368b3d77ac8ecda7
NirmalHettiarachchi <nirmalhettiarachchi5@gmail.com>
2024-01-16 07:28:45 +0530

    Edited vehicle details window; added a save btn

--- app/src/main/java/eu/tutorials/roadrescuecustomer/views/DashboardScreen.kt
+++ app/src/main/java/eu/tutorials/roadrescuecustomer/views/DashboardScreen.kt
@@ -389,7 +389,15 @@ fun RequestServiceScreen(
             ) {
                 showIssueDetailsWindow = true
             }
-            FillDetailsButton(detailButtonName = "Vehicle Details") {
+            FillDetailsButton(detailButtonName = "Vehicle Details" +
+                    if (serviceRequestViewModel.vehicleModel.value.isEmpty()) ""
+                    else ": ${serviceRequestViewModel.vehicleMake.value + 
+                            " " + 
+                            serviceRequestViewModel.vehicleModel.value + 
+                            " {" +
+                            serviceRequestViewModel.fuelType.value +
+                            "}"
+                    } ") {
                 showVehicleDetailsWindow = true
             }
 
@@ -479,7 +487,7 @@ fun RequestServiceScreen(
     }
 
     if(showVehicleDetailsWindow) {
-        VehicleDetailsWindow {
+        VehicleDetailsWindow (serviceRequestViewModel) {
             showVehicleDetailsWindow = false
         }
     }
