8ffde0b1c3c52bc0b3ce4eb060a4691a3f2a4acd
TharinduDakshina <tharindudakshina527@gmail.com>
2024-04-16 16:51:59 +0530

    update

--- app/src/main/java/com/example/garage/views/SidebarContent.kt
+++ app/src/main/java/com/example/garage/views/SidebarContent.kt
@@ -17,6 +17,10 @@ import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
@@ -63,7 +67,11 @@ fun SidebarContent(menuClicked:()->Unit){
                 .fillMaxWidth()
                 .fillMaxHeight(0.9f)
         ) {
-            SidebarButton(buttonName="Open",verticalPadding=8, onClick = {})
+            var garageOpenClosedStatus by remember { mutableStateOf("Closed") }
+
+            SidebarButton(buttonName=garageOpenClosedStatus,verticalPadding=8, onClick = {
+                garageOpenClosedStatus=chaneState(garageOpenClosedStatus)
+            })
 
             SidebarButton(buttonName="Activities",verticalPadding=8, onClick = {})
 
@@ -78,6 +86,15 @@ fun SidebarContent(menuClicked:()->Unit){
 
 }
 
+fun chaneState(garageOpenClosedStatus: String):String {
+    var stastus=""
+    if (garageOpenClosedStatus == "Closed") {
+        stastus="Open"
+    }else{
+        stastus="Closed"
+    }
+    return stastus
+}
 
 
 @Composable
@@ -86,9 +103,7 @@ fun SidebarButton(buttonName:String,verticalPadding:Int,onClick:()->Unit){
 
     Button(
 
-        onClick = { },
-
-
+        onClick = onClick ,
 
         elevation = ButtonDefaults.buttonElevation(defaultElevation = 4.dp),
 
